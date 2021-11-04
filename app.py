2c4a40d3ab3006932aa4c85267b7ad6a81cab967
MRandyAnugerah <improud.ann@gmail.com>
2021-11-04 08:58:02 +0700

    add main.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import pandas as pd
-
-def landpage():
-    st.title("To Do App My Work")
-
-    menu = ["Create", "Read", "Update", "Delete", "About"]
-    choice = st.sidebar.selectbox("Menu", menu)
-import streamlit as st
-import pandas as pd
 from database import create_data, create_table
 
 def landpage():
@@ -27,21 +19,6 @@ def landpage():
         if st.button("Create Work"):
             create_data(work, work_status, work_due_date)
             st.success("Successfully Create new Work: {}".format(work))
-        
-    elif choice == "Read":
-        st.subheader("View My Work")
-
-    elif choice == "Update":
-        st.subheader ("Edit My Work")
-
-    elif choice == "Delete":
-        st.subheader ("Delete My Work")  
-          
-    else:
-        st.subheader("Check Me")
-
-    if choice == "Create":
-        st.subheader("Add Work")
         
     elif choice == "Read":
         st.subheader("View My Work")
